303a9ece59a21a39702cb2f08a965fcf1bcc9e93
cristielle <cristielle.santos94@gmail.com>
2024-07-26 17:04:11 -0300

    feat: condicoes

---
+++ aula 11 - condicoes/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+
+if(vel > 60){ //condição simples
+    console.log('Você ultrapassou a velocidade, foi multado')
+}
+
+console.log('Use cinto de segurança')
