ff38c63394b99c63c1c54e16b4e10335f9b26f9c
cristielle <cristielle.santos94@gmail.com>
2024-08-05 17:39:58 -0300

    feat: estruturas de repeticao

---
+++ aula 13 - repeticoes/script.js
@@ -0,0 +1,18 @@
+//teste lógico no final
+var contador = 1
+
+do{
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 6)
+
+
+/*
+//teste lógico no início
+var contador = 1
+
+while (contador <= 6){
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
