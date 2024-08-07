71f34bbc6cae47b04894033f6b049ce1af4d63dd
cristielle <cristielle.santos94@gmail.com>
2024-08-07 16:00:11 -0300

    feat: exercicio somatoria

---
+++ aula 14 - repeticoes II/aula 14 - ex/ex018/script.js
@@ -0,0 +1,33 @@
+// Exercicio 01
+
+var contador = 0
+var soma = 0.0
+
+while(contador < 20){
+    var valor = parseFloat(contador + 1)
+    soma += valor
+    contador++
+}
+
+console.log(`A soma dos valor é: ${soma}`)
+
+
+// Exercicio 02
+
+var contador = 0
+var soma = 0.0
+
+do{
+    var valor = parseFloat(contador + 1)
+    soma += valor
+    contador++
+    console.log(`A soma dos valores é ${soma}`)
+    
+
+} while (contador < 20)
+
+
+
+
+
+    
